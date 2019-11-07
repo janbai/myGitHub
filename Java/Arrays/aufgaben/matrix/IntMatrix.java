8a718a1226ce518bf8087cd787f5507bf4b09c1d
CC-Student <CC-Student@192.168.0.45>
2019-11-07 22:09:27 +0100

    update

--- Java/Arrays/aufgaben/matrix/IntMatrix.java
+++ Java/Arrays/aufgaben/matrix/IntMatrix.java
@@ -29,42 +29,100 @@ public class IntMatrix {
 				values[i][j] = random.nextInt(max-min)+ min;
 			}
 		}
-}
-	public static IntMatrix setValueInMatrix(IntMatrix array, int r, int c , int val){
-		IntMatrix back = new IntMatrix(5,3);
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 3; j++) {
-				
-			back.values[i][j] = array.values[i][j];
-			if(i==r && j==c) {back.values[i][j] = val;}
-				
-			}
-		}
-		return back;
 	}
 	
+	public void set(int row, int column, int newValue) {
+		values[row][column] = newValue;
+	}
+		
+	public int get(int row, int column) {
+		return values[row][column];
+	}
+	public int getCountRows() {
+		return values.length;
+	}
 	
-	@Override
-	public String toString() {
-		// todo: StringBuilder einsetzen
+	public int getCountColumns() {
+		return values[0].length;
+	}
+	
+// equals -----------------------------------------------
+	public boolean equals(Object obj) { 
+		if (!(obj instanceof IntMatrix)) {
+			return false;
+		}
+		IntMatrix m2 = (IntMatrix)obj;
+		return equals(m2);
+	}
+	
+public boolean equals(IntMatrix m2) { // Überladen der Methode mit der Signatur equals(Object)
 		
-		String text = "";
+		if(m2==null) {
+			return false;
+		}
 		
-		for (int i = 0; i < values.length; i++) {
-			for (int j = 0; j < values[i].length; j++) {
-				text = text + values[i][j];
-				
-				if(j < values[i].length-1) {
-					text = text + ", "; 
+		if( getCountRows() != m2.getCountRows() || getCountColumns() != m2.getCountColumns() ) {
+			return false;
+		}
+	
+		for (int row = 0; row < getCountRows(); row++) {
+			for (int col = 0; col < getCountColumns(); col++) {
+				if( get(row, col) != m2.get(row, col) ) {
+					return false;
 				}
 			}
-			text = text + '\n';
 		}
 		
-		return text;
+		return true;
 	}
-	
-	
+
+
+// Print Object Array ------------------------------------------
+			static void printfore (IntMatrix array) {
+				for (int[] subArray : array.values) {
+					for (int x : subArray) {
+						System.out.print(x + " ");
+					}
+					System.out.println();
+				}
+			}// end of printfore
+			
+			static void printfor (IntMatrix array) {
+				
+				for (int i = 0; i < array.values.length; i++) {
+					for (int j = 0; j < array.values[i].length; j++) {
+						
+						if(j < array.values[i].length-1) {
+							System.out.print(array.values[i][j] + ", ");
+							}else {
+								System.out.print(array.values[i][j]);
+							}
+						}
+					System.out.println();
+				}
+			}// end of printfor
+			
+			@Override
+			public  String toString() {
+				// todo: StringBuilder einsetzen
+				
+				String text = "";
+				
+				for (int i = 0; i < values.length; i++) {
+					for (int j = 0; j < values[i].length; j++) {
+						text = text + values[i][j];
+						
+						if(j < values[i].length-1) {
+							text = text + ", "; 
+						}
+					}
+					text = text + '\n';
+				}
+				
+				return text;
+			}//end of String toString
+			
+//----------------------------------------------------------	
 
 	
 }
