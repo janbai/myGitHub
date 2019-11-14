61b6ede10841424a06241200392848f59425394a
CC-Student <CC-Student@192.168.0.45>
2019-11-14 14:19:30 +0100

    update

---
+++ Java/src/tecTacToe/TicTacToe_main.java
@@ -0,0 +1,60 @@
+package tecTacToe;
+
+import java.util.Scanner;
+
+public class TicTacToe_main {
+	
+	private static int move() {
+
+		 Scanner scanner = new Scanner(System.in);
+		
+		 int p= scanner.nextInt();
+		 return p;
+	}
+	private static int[] position(int x) {
+		int[] pos= new int[2];
+		int y= 0;
+		for (int i = 0; i < 3; i++) { 
+		    for (int j = 0; j < 3; j++) { 
+		    	y = y + 1; 
+		    	if(y ==x) {
+		    		pos[0] = i;
+		    		pos[1] = j;
+		    		
+		    			break;
+		    	}
+		     } 
+		}
+	return pos; 
+	}
+	
+	
+	public static void main(String[] args) {
+		TicTacToe game = new TicTacToe(); 
+		game.printwelcome();
+		 int player= 1;
+		while (!game.isBoardFull()){
+			System.out.print("Spieler(" + player + ") wählen : ");
+			
+			int p= move();	
+			int row = position(p)[0];
+			int col = position(p)[1];
+			boolean b = game.placeMark(row,col); 
+			game.changePlayer();
+			game.printBoard(); 
+			
+			if (game.checkForWin()) { 
+				System.out.println("*******************************************"); 
+				System.out.println("**  Wir haben einen Sieger! Glückwunsch! **"); 
+				System.out.println("*******************************************"); 
+					System.exit(0); 
+						} else if (game.isBoardFull()) { 
+							System.out.println("Anscheinend haben wir ein Unentschieden!"); 
+								System.exit(0);
+			}
+			 
+			if (b) {if (player== 1 ){player=2;}else { player=1;}}}
+ 								
+}// end of main
+
+}
