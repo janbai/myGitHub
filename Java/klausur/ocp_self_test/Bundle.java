a86902abac512de85d0b6ae907137ecfb194f10e
HJAN181 <HJAN181@PC16141418.cc-college.gecs.loc>
2020-03-06 12:51:44 +0100

    update

---
+++ Java/klausur/ocp_self_test/Bundle.java
@@ -0,0 +1,6 @@
+package ocp_self_test;
+public class Bundle extends java.util.ListResourceBundle {
+protected Object[][] getContents() {
+return new Object[][] { { "123", 456 } };
+}
+}
---
+++ Java/klausur/ocp_self_test/KeyValue.java
@@ -0,0 +1,49 @@
+package ocp_self_test;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.*;
+
+class ToDos{
+String day;
+ToDos(String d) { day = d; }
+public boolean equals(Object o) {
+return ((ToDos)o).day.equals(this.day);
+}
+//public int hashCode() { return 9; }
+}
+public class KeyValue {
+public static void main(String[] args) {
+	
+	ArrayList<Integer> input = null;
+	ArrayList<Integer> output = null;
+	ArrayList<Integer> input1 = null;
+	List<Integer> output2 = null;
+	ArrayList<Integer> input2 = null;
+	List<Number> output3 = null;
+	 List<Number> input3 = null;
+	ArrayList<Integer> output4 = null;
+	 List<Number> input4 = null;
+	List<Number> output5 = null;
+	 List<Integer> input5 = null;
+	List<Integer> output6 = null;
+	
+	
+	}
+public static <E extends Number> List<E> process(List<E> nums){
+	return nums;
+	
+}
+}
